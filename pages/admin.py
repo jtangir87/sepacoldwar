b55ba17070f0f3c6da8edcb4a26b0b9ebb713682
Jared Tangir <jaredtangir@Jareds-MacBook-Pro-3.local>
2020-10-13 18:22:16 -0400

    add NADCPhotoComment admin filter

--- pages/admin.py
+++ pages/admin.py
@@ -12,6 +12,9 @@ from .models import (
 from django.conf import settings
 
 # Register your models here.
+class NADCPhotoCommentAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "approved")
+    list_filter = ["approved"]
 
 
 class RemoveAddPersmissionAdmin(admin.ModelAdmin):
@@ -27,7 +30,7 @@ if settings.DEBUG == False:
     admin.site.register(EventDescriptions, RemoveAddPersmissionAdmin)
     admin.site.register(NADCPhotoPage, RemoveAddPersmissionAdmin)
     admin.site.register(NADCPhoto)
-    admin.site.register(NADCPhotoComment)
+    admin.site.register(NADCPhotoComment, NADCPhotoCommentAdmin)
 
 else:
     admin.site.register(DynamicFlightSimulatorPage)
@@ -37,4 +40,4 @@ else:
     admin.site.register(EventDescriptions)
     admin.site.register(NADCPhotoPage)
     admin.site.register(NADCPhoto)
-    admin.site.register(NADCPhotoComment)
+    admin.site.register(NADCPhotoComment, NADCPhotoCommentAdmin)
