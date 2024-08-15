aca61ad95cdb7848ae531b6d2ff0cfb4faad343d
jtangir87 <37902751+jtangir87@users.noreply.github.com>
2024-08-15 18:07:30 -0400

    august 2024 updates

--- pages/urls.py
+++ pages/urls.py
@@ -126,6 +126,12 @@ urlpatterns = [
             template_name="pages/projects/nadc_25th_reunion.html"),
         name="nadc_25th_reunion",
     ),
+    path(
+        "projects/naval-air-development-center/alumni-reception",
+        TemplateView.as_view(
+            template_name="pages/projects/nadc_alumni_reception.html"),
+        name="nadc_alumni_reception",
+    ),
     ## WGNAS ##
     path("projects/willow-grove-naval-air-station", wgnas_page, name="wgnas"),
     path(
